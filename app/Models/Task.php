1d08cbed7767cd5022bf25198b6049aa3db0e6e1
TechnoPreacher <nik.nik.sulima@gmail.com>
2022-07-05 02:02:11 +0300

    models

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+
+    public function labels()
+    {
+        return $this->belongsToMany(Label::class);
+    }
 }
